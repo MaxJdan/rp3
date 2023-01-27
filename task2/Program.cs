918b6421fb7c967cb966c9279ee3341888a28dbf
Max Jdanov <maxzhdanov@list.ru>
2023-01-27 16:00:47 +0300

    made the first code on c#

---
+++ task2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+int sqr = number * number;
+Console.WriteLine("квадрат числа " + number + " равен= " + sqr);
