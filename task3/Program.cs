ac2a869c32f6e3dd7558f78548b2f4273cd8fb37
Max Jdanov <maxzhdanov@list.ru>
2023-01-27 16:39:26 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    //Console.Clear();
+    Console.Write("введите число: ");
+    int number1 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    int number2 = int.Parse(Console.ReadLine());
+    if (number1 == number2 * number2)
+    {
+      Console.WriteLine( number1 + " является квадратом " + number2);
+      }
+    else 
+    {
+      Console.WriteLine( number1 + " не является квадратом " + number2);
+    }  
+  
+   
+  }
+}
